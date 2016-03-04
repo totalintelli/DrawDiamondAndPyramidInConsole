c14b4077b5410adcbce33b006d526c17942dc083
totalintelli <blueintelli@gmail.com>
2016-03-04 10:38:18 +0900

    다이아몬드와 피라미드 이미지를 그리는 함수를 구현

--- DrawDiamond.cs
+++ DrawDiamond.cs
@@ -15,6 +15,13 @@ namespace DrawDiamondInConsole
 {
     class DrawDiamond
     {
+        public enum ImageType
+        {
+            Diamond,
+            EmptyDiamond,
+            Pyramid,
+            ReversePyramid
+        }
         static void Main(string[] args)
         {
             int number;
@@ -22,14 +29,43 @@ namespace DrawDiamondInConsole
             int row;
             int column;
             DrawDiamond drawDiamond = new DrawDiamond();
+            ImageType imgType = ImageType.Diamond;
+            int imgTypeNumber = 0;
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("1. 다이아몬드");
+            Console.WriteLine("2. 빈 다이아몬드");
+            Console.WriteLine("3. 피라미드");
+            Console.WriteLine("4. 뒤집힌 피라미드");
+            Console.WriteLine("보고 싶은 문양을 고르세요.");
+            Console.WriteLine("---------------------------");
+            imgTypeNumber = Int32.Parse(Console.ReadLine());
+
+            switch(imgTypeNumber)
+            {
+                case 1:
+                    imgType = ImageType.Diamond;
+                    break;
+                case 2:
+                    imgType = ImageType.EmptyDiamond;
+                    break;
+                case 3:
+                    imgType = ImageType.Pyramid;
+                    break;
+                case 4:
+                    imgType = ImageType.ReversePyramid;
+                    break;
+                default:
+                    break;
+            }
 
             // 수를 사용자로부터 입력받는다.
-            number = drawDiamond.Input();
+            number = drawDiamond.Input(imgType);
 
             if (number > 0)
             {
                 //다이아몬드 이미지를 만든다.
-                drawDiamond.MakeDiamondImage(number, out image, out row, out column);
+                //drawDiamond.MakeDiamondImage(number, out image, out row, out column);
 
                 // 속이 빈 다이아몬드 이미지를 만든다
                 //drawDiamond.MakeEmptyDiamondImage(number, out image, out row, out column);
@@ -40,6 +76,8 @@ namespace DrawDiamondInConsole
                 // 뒤집힌 피라미드 이미지를 만든다
                 //drawDiamond.MakeReversePyramidImage(number, out image, out row, out column);
 
+                //위의 함수 네 개를 합칠 것
+                drawDiamond.MakeDiamondImage(number, imgType, out image, out row, out column);
 
                 // 이미지를 출력한다.
                 drawDiamond.Output(image, row, column);
@@ -58,21 +96,27 @@ namespace DrawDiamondInConsole
         /*
         함수 이름: Input
         기    능: 수를 입력받는다.
-        입    력: 없음
+        입    력: 이미지 타입 번호
         출    력: 수
         */
-        int Input()
+        int Input(ImageType imgType)
         {
-            int number; // 수
+            int number = 0; // 수
 
-            // 프로그램 설명을 출력한다.
-            Console.Write("Program for displaying pattern of *.\nEnter the maximum number of *: ");
-            number = Int32.Parse(Console.ReadLine());
-            Console.Write("\nHere is the Diamond of Stars\n");
+            if(imgType == ImageType.Diamond || imgType == ImageType.EmptyDiamond)
+            {
+                // 프로그램 설명을 출력한다.
+                Console.Write("Program for displaying pattern of *.\nEnter the maximum number of *: ");
+                number = Int32.Parse(Console.ReadLine());
+                Console.Write("\nHere is the Diamond of Stars\n");
+            }
+            else if(imgType == ImageType.Pyramid || imgType == ImageType.ReversePyramid)
+            {
+                // 프로그램 설명을 출력한다.
+                Console.Write("마지막에 출력할 별의 수를 입력하세요! 피라미드를 만듭니다.");
+                number = Int32.Parse(Console.ReadLine());
+            }
 
-            // 프로그램 설명을 출력한다.
-            //Console.Write("마지막에 출력할 별의 수를 입력하세요! 피라미드를 만듭니다.");
-            //number = Int32.Parse(Console.ReadLine());
 
             return number;
         }
@@ -301,6 +345,141 @@ namespace DrawDiamondInConsole
                 start++;
                 end--;
                 i--;
+            }
+        }
+
+        void MakeDiamondImage(int number, ImageType imgType, out char[,] image, out int row, out int column)
+        {
+            int start = 0; // 별표가 들어가는 시작 위치(Ordinal number), Ordinal number는 프로그래밍에서 0부터 시작한다. 
+            int end;       // 별표가 들어가는 마지막 위치(Ordinal number)
+            int i;         // 줄의 위치(Ordinal number) vs row -> 줄의 개수 -> Cardinal number -> 1부터 시작한다.
+            int j;         // 열의 위치(Ordinal number) vs column -> 열의 개수 -> Cardinal number
+
+            image = null;
+            row = 0;
+            column = 0;
+
+            if(imgType == ImageType.Diamond || imgType == ImageType.EmptyDiamond)
+            {
+                // 이미지를 만들 준비를 한다.
+                row = number * 2 - 1;
+                column = row;
+                end = column - 1;
+                image = new char[row, column];
+
+                // 이미지를 만든다.
+                i = number - 1;
+                while (i >= 0)
+                {
+                    j = 0;
+                    while (j < column)
+                    {
+                        if(imgType == ImageType.EmptyDiamond)
+                        {
+                            if (j == start || j == end)
+                            {
+                                image[i, j] = '*';
+                                image[column - i - 1, j] = '*';
+                            }
+                            else
+                            {
+                                image[i, j] = ' ';
+                                image[column - i - 1, j] = ' ';
+                            }
+
+                        }
+                        else
+                        {
+                            if (j >= start && j <= end)
+                            {
+                                image[i, j] = '*';
+                                image[column - i - 1, j] = '*';
+                            }
+                            else
+                            {
+                                image[i, j] = ' ';
+                                image[column - i - 1, j] = ' ';
+                            }
+                        }
+                        j++;
+
+                    }
+                    start++;
+                    end--;
+                    i--;
+                }
+            }
+
+
+            if(imgType == ImageType.Pyramid || imgType == ImageType.ReversePyramid)
+            {
+                // 수가 짝수이면 별의 개수에 맞춘다.
+                if (number % 2 == 0)
+                {
+                    number++;
+                }
+
+                // 줄의 개수를 구한다.
+                row = (number + 1) / 2;
+
+                // 열의 개수를 구한다.
+                column = number;
+
+                // 마지막 위치를 구한다.
+                end = number - 1;
+
+                // 이미지를 구한다.
+                image = new char[row, column];
+
+
+                if (imgType == ImageType.Pyramid)
+                {
+                    i = row - 1;
+                    while (i >= 0)
+                    {
+                        j = 0;
+                        while (j < column)
+                        {
+
+                            if (j >= start && j <= end)
+                            {
+                                image[i, j] = '*';
+                            }
+                            else
+                            {
+                                image[i, j] = ' ';
+                            }
+                            j++;
+                        }
+                        start++;    
+                        end--;
+                        i--;
+                    }
+                }
+                else
+                {
+                    i = 0;
+                    while (i < row)
+                    {
+                        j = 0;
+                        while (j < column)
+                        {
+                            if (j >= start && j <= end)
+                            {
+                                image[i, j] = '*';
+                            }
+                            else
+                            {
+                                image[i, j] = ' ';
+                            }
+                            j++;
+                        }
+                        start++;
+                        end--;
+                        i++;
+                    }
+                }
+                    
             }
         }
     }
